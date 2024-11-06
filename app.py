bc7e7201cc9d76450be476708321e0a0ddad0fd4
shaikhilyas <shaikhilyasit@gmail.com>
2024-11-06 23:26:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
     return 'Hello GSMC CAAS Team!'
+    return 'Welcome'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8082)
